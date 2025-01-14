a1befd1a1eff5337770c29217b47f7bac19ba5bf
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-01-14 10:52:13 -0500

    Part 1 Determine winner

--- index.js
+++ index.js
@@ -27,9 +27,47 @@ document.getElementById("draw-cards").addEventListener("click", handleDrawCards)
 /**
  * Challenge:
  * 
- * Display the images of the 2 cards you drew in the browser.
- * Probably best to use `innerHTML` to insert a couple <img> elements
- * on the page.
+ * Try to determine which of the 2 cards is the "winner" (has higher value)
+ * Aces are the card with the highest "score"
+ * 
+ * In parts:
+ * 
+ * 1. Create a function that takes 2 card objects as parameters, 
+ * `card1` and `card2`. These card objects have a property called
+ * `value`, which can be any one of the following strings, in
+ * order of rising "score":
+ * 
+ * "2", "3", "4", "5", "6", "7", "8", "9", 
+ * "10", "JACK", "QUEEN", "KING", "ACE"
+ * 
+ * I.e. "2" is the lowest score and "ACE" is the highest.
+ * 
+ * The function should determine which of the 2 cards (`card1`
+ * or `card2`) has the higher score, or if they have the same score.
+ * 
+ * Log which card wins (or "It's a tie!" 
+ * if they're the same) to the console
  */
+
+function compareCards(card1, card2) {
+    const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"];
+
+    const card1Value = values.indexOf(card1.value);
+    const card2Value = values.indexOf(card2.value);
+    // console.log(card1Value)
+    // console.log(card2Value)
+
+    if (card1Value > card2Value) {
+        console.log(`${card1.value} wins!`)
+
+    } else if (card1Value < card2Value) {
+        console.log(`${card2.value} wins!`)
+    } else {
+        console.log("It's a tie!")
+    }
+}
+
+compareCards({ value: "ACE" }, { value: "KING" });
+
 
 
